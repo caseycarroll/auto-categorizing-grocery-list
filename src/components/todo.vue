00cd98355be6078048c154fce8c423167aef9b5e
Casey Carroll <aiyakc@gmail.com>
2026-01-11 22:48:21 -0800

    todo styles

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -10,8 +10,29 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-    <li>
+    <li class="todo-item">
       <input type="checkbox" :id="props.name" :checked="props.checked" />
       <label :for="props.name">{{props.name}}</label>
     </li>
 </template>
+
+<style scoped>
+.todo-item {
+    display: flex;
+    align-items: center;
+    gap: var(--space-md);
+    padding: var(--space-md);
+    border: 1px dashed var(--color-primary-foreground);
+    border-radius: var(--radius-lg);
+    position: relative;
+
+    label {
+        flex-grow: 1;
+        &:after {
+            content: '';
+            inset: 0;
+            position: absolute;
+        }
+    }
+}
+</style>
