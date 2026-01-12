b2279cb88f2b87e5edb8db28d90068b50cfd3730
Casey Carroll <aiyakc@gmail.com>
2026-01-11 22:28:26 -0800

    styles

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -10,6 +10,8 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-  <input type="checkbox" id="test" :checked="props.checked" />
-  <label for="test">{{props.name}}</label>
+    <li>
+      <input type="checkbox" :id="props.name" :checked="props.checked" />
+      <label :for="props.name">{{props.name}}</label>
+    </li>
 </template>
