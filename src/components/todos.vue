6ff76cbe222190ddebf8e52562deee67273889c6
Casey Carroll <aiyakc@gmail.com>
2026-01-16 22:00:37 -0800

    checkpoint

---
+++ src/components/todos.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Todo from './todo.vue';
+
+interface TodoItem {
+    checked: boolean;
+    name: string;
+}
+
+const props = defineProps<{ todos: TodoItem[] }>();
+</script>
+
+<template>
+  <ul role="list" class="flow">
+    <Todo v-for="todo in props.todos" :key="todo.name" :checked="todo.checked" :name="todo.name" />
+  </ul>
+</template>
