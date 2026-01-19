1e3db81913bed8e813d922019b8d0b00b78f6c83
Casey Carroll <cacarroll@ebay.com>
2026-01-19 10:57:17 -0800

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import vue from '@astrojs/vue';
 
 import db from '@astrojs/db';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), db()]
+  integrations: [vue(), db()],
+  adapter: netlify()
 });
