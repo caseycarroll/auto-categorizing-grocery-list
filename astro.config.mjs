4b55fdf363cd58567a8ae6f916e84c811fa23119
Casey Carroll <cacarroll@ebay.com>
2026-01-18 23:18:10 -0800

    Revert "add astro icon"
    This reverts commit c68cfc3a482e7b016031fcfa3ae63212148ec489.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+
 import vue from '@astrojs/vue';
+
 import db from '@astrojs/db';
-import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), db(), icon()]
+  integrations: [vue(), db()]
 });
