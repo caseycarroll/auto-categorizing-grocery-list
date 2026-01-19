4a85b5c44fbe470c926c3bd2ee378fe7e97f0c96
Casey Carroll <cacarroll@ebay.com>
2026-01-18 16:09:44 -0800

    add basic validation

--- src/components/sign-up-form.vue
+++ src/components/sign-up-form.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
 import { signUp } from '../libs/auth-client';
 
-const name = defineModel('name');
-const email = defineModel('email');
-const password = defineModel('password');
+const name = defineModel<string>('name', {required: true});
+const email = defineModel<string>('email', {required: true});
+const password = defineModel<string>('password', {required: true});
 
 
 async function onSubmit() {
-    console.log(name.value, email.value, password.value)
+    if(!name || !email || !password) return;
     const {data, error} = await signUp.email({
         name: name.value,
         email: email.value,
         password: password.value,
     })
     if(error) console.log('error', error)
-    console.log(data)
     window.location.href = '/grocery-list'
 }
 </script>
@@ -27,15 +26,15 @@ async function onSubmit() {
         <form @submit.prevent="onSubmit">
             <div>
                 <label for="name">Name</label>
-                <input v-model="name" id="name" type="text" />
+                <input v-model="name" id="name" type="text" required />
             </div>
             <div>
                 <label for="email">Email</label>
-                <input v-model="email" id="email" type="email" />
+                <input v-model="email" id="email" type="email" required />
             </div>
             <div>
                 <label for="pass">Password</label>
-                <input v-model="password" id="pass" type="password" />
+                <input v-model="password" id="pass" type="password" required />
             </div>
             <div class="repel submit-btn">
                 <button id="sign-in" type="submit"><span>Sign up</span></button>
