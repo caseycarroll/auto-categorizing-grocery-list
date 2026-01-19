c68cfc3a482e7b016031fcfa3ae63212148ec489
Casey Carroll <cacarroll@ebay.com>
2026-01-18 23:11:45 -0800

    add astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vue from '@astrojs/vue';
-
 import db from '@astrojs/db';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), db()]
+  integrations: [vue(), db(), icon()]
 });
